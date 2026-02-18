f78740b2dffdc79f9297b5e4e73b5d019494d950
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-02-18 16:16:20 +0530

    init(backend): Initialized the backed with springboot

---
+++ server/src/main/java/com/example/shapedesigner/ShapedesignerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapedesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapedesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapedesignerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/shapedesigner/ShapedesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shapedesigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapedesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
